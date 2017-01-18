216650e5f009c4e9435f44e249411378e8523886
Mehmet Uygun <mehmet.uygun@outlook.com>
2017-01-18 13:20:00 +0000

    Create queen model

---
+++ src/Model/Queen.php
@@ -0,0 +1,27 @@
+<?php
+
+	namespace Model;
+
+	use Bee;
+
+	/**
+	* The queen model class.
+	*/
+	class Queen extends Bee
+	{
+		
+		/**
+		 * The constructor of queen model
+		 * @param integer $lifeSpan 
+		 * @param integer $life     
+		 * @param integer $hitPoint
+		 */
+		function __construct($lifeSpan = 100, $life = 1, $hitPoint = 10)
+		{
+			$this->lifeSpan = $lifeSpan;
+			$this->life 	= $life;
+			$this->hitPoint = $hitPoint;
+		}
+	}
+
+?>
