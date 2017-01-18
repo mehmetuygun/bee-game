94c99807f2459030facfea5e03615c76ebf732c1
Mehmet Uygun <mehmet.uygun@outlook.com>
2017-01-18 13:12:39 +0000

    Create bee model

---
+++ src/Model/Bee.php
@@ -0,0 +1,31 @@
+<?php
+
+	namespace Model;
+
+	/**
+	* The Bee abstract class.
+	*/
+  	abstract class ClassName
+	{	
+
+		/**
+		 * The total points of bee. When it is deducted in zero. The bee loses its life.
+		 * @var integer
+		 */
+		public $lifeSpane;
+
+		/**
+		 * The amount of life that bee has.
+		 * @var [type]
+		 */
+		public $life;
+
+		/**
+		 * The hit point that will deduct from bee's lifeSpan.
+		 * @var [type]
+		 */
+		public $hitPoint
+
+	}
+
+?>
