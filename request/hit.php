843af04a63b3586a59f96a9db8e8847d93270522
Mehmet Uygun <mehmet.uygun@outlook.com>
2017-01-18 17:46:08 +0000

    Update hit request

--- request/hit.php
+++ request/hit.php
@@ -2,13 +2,16 @@
 
 	require '../vendor/autoload.php';
 
+	//Get game object from stored file
 	$storedGame = file_get_contents('../store');
 
+	//Convert stored file into object
 	$Game = unserialize($storedGame);
 
 	$Game->hit();
 
-	if ($Game->queen->isDead())
+	//Check the if game is over
+	if ($Game->isOver())
 	{
 		echo json_encode([
 				"status" => 0,
@@ -18,6 +21,7 @@
 	{
 		echo json_encode([
 				"status" => 1,
+				"round" => $Game->round,
 				"bees" => [
 					"Queen" => ["life" => $Game->queen->life, "lifeSpan" => $Game->queen->lifeSpan],
 					"Worker" => ["life" => $Game->worker->life, "lifeSpan" => $Game->worker->lifeSpan],
@@ -26,7 +30,8 @@
 			]);
 	}
 
+	//Set the changed game object into store file
 	$storedGame = serialize($Game);
-	file_put_contents('store', $storedGame);
+	file_put_contents('../store', $storedGame);
 
 ?>
