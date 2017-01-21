2e29e361b2e08ec26d747aa8da9952cb7253666f
Mehmet Uygun <mehmet.uygun@outlook.com>
2017-01-21 18:27:02 +0000

    Convert public property to protected

--- tests/BeeTest.php
+++ tests/BeeTest.php
@@ -11,54 +11,39 @@ class BeeTest extends \PHPUnit_Framework_TestCase
 {
 	public function testDroneBee()
 	{
-		$game = new Game();
-
 		$droneBee = new Drone(5, 5, 10);
 
-		$game->addBee($droneBee);
-
-		$this->assertEquals(5, $game->getBee('Drone')->lifeSpan);
-		$this->assertEquals(5, $game->getBee('Drone')->life);
-		$this->assertEquals(10, $game->getBee('Drone')->hitPoint);
+		$this->assertEquals(5, $droneBee->getLifeSpan());
+		$this->assertEquals(5, $droneBee->getLife());
+		$this->assertEquals(10, $droneBee->getHitPoint());
 	}
 
 	public function testQueenBee()
 	{
-		$game = new Game();
-
 		$queenBee = new Queen(5, 5, 10);
 
-		$game->addBee($queenBee);
-
-		$this->assertEquals(5, $game->getBee('Queen')->lifeSpan);
-		$this->assertEquals(5, $game->getBee('Queen')->life);
-		$this->assertEquals(10, $game->getBee('Queen')->hitPoint);
+		$this->assertEquals(5, $queenBee->getLifeSpan());
+		$this->assertEquals(5, $queenBee->getLife());
+		$this->assertEquals(10, $queenBee->getHitPoint());
 	}
 
 	public function testWorkerBee()
 	{
-		$game = new Game();
-
 		$workerBee = new Worker(5, 5, 10);
 
-		$game->addBee($workerBee);
-
-		$this->assertEquals(5, $game->getBee('Worker')->lifeSpan);
-		$this->assertEquals(5, $game->getBee('Worker')->life);
-		$this->assertEquals(10, $game->getBee('Worker')->hitPoint);
+		$this->assertEquals(5, $workerBee->getLifeSpan());
+		$this->assertEquals(5, $workerBee->getLife());
+		$this->assertEquals(10, $workerBee->getHitPoint());
 	}
 
 	public function testSubHitPoint()
 	{
-		$game = new Game();
-
 		$workerBee = new Worker(5, 5, 10);
 
-		$this->assertEquals(5, $workerBee->life);
+		$this->assertEquals(5, $workerBee->getLife());
 
 		$workerBee->subHitPoint();
 
-		$this->assertEquals(4, $workerBee->life);
+		$this->assertEquals(4, $workerBee->getLife());
 	}
-
 }
