74a92fc53d43956432f862e53b8b9075c189a0fb
Mehmet Uygun <mehmet.uygun@outlook.com>
2017-01-21 12:53:57 +0000

    Create game test

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App\Tests
+namespace MehmetUygun\BeeGame\Tests;
 
-class GameTest extends \PHPUnit_Framework_Test_Case
+use MehmetUygun\BeeGame\Game;
+use MehmetUygun\BeeGame\Model\Drone;
+use MehmetUygun\BeeGame\Model\Queen;
+use MehmetUygun\BeeGame\Model\Worker;
+
+class GameTest extends \PHPUnit_Framework_TestCase
 {
-	public function testAddsBee()
+	public function testAddsAndGetsBee()
 	{
 		$game = new Game();
 
@@ -12,18 +17,76 @@ class GameTest extends \PHPUnit_Framework_Test_Case
 
 		$game->addBee($bee);
 
-		$this->assertContains($bee, $game->bees);
-		$this->assertCount(1, $game->bees);
-	}
-}
+		$this->assertInstanceOf(Drone::class, $game->getBee('Drone'));
 
-class BeeTest extends \PHPUnit_Framework_Test_Case
-{
-	public function testHit()
+		$game->removeBee($bee);
+
+		$this->assertNotInstanceOf(Drone::class, $game->getBee('Drone'));
+
+	}
+
+	public function testIsOver()
 	{
-		$bee = new Drone(5, 3, 2);
-		$bee->hit();
+		$game = new Game();
 
-		$this->assertEquals(3, $bee->lifeSpan);
+		$this->assertTrue($game->isOver());
+
+		$Queen = new Queen();
+		$game->addBee($Queen);
+
+		$this->assertFalse($game->isOver());
+	}
+
+	public function testRemoveBee()
+	{
+		$game = new Game();
+
+		$bee = new Drone();
+
+		$game->addBee($bee);
+
+		$this->assertInstanceOf(Drone::class, $game->getBee("Drone"));
+
+
+		$game->removeBee($bee);
+
+		$this->assertNull($game->getBee("Drone"));
+	}
+
+	public function testHitBeeAndSelectBee()
+	{
+		$game = new Game();
+
+		$queenBee = new Queen(100, 5, 5);
+		$bee = new Drone(10, 5, 5);
+		$workerBee = new Worker(1, 5, 5);
+		
+		$game->addBee($workerBee);
+		$game->addBee($bee);
+		$game->addBee($queenBee);
+
+		$game->hitBee($bee);
+		$game->hitBee($workerBee);
+
+		$this->assertEquals(5, $game->getBee("Drone")->lifeSpan);
+
+		$this->assertEquals(4, $game->getBee('Worker')->life);
+
+	}
+
+	public function testGetRound()
+	{
+		$game = new Game();
+
+		$queenBee = new Queen(10, 5, 5);
+
+		$game->addBee($queenBee);
+
+		$this->assertEquals(0, $game->getRound());
+
+		$game->hitBee($queenBee);
+
+		$this->assertEquals(1, $game->getRound());
 	}
 }
+
